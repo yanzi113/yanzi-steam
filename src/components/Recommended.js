8731add807845b490aef849de5abef90c4dfc3ed
yanzi113 <yanzi7310v0@gmail.com>
2024-08-04 01:51:04 +0800

    更新更新

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Carousel from "./Carousel.js";
 export default class Recommended extends Component {
   render() {
     return (
-        <Carousel></Carousel>        
+        <Carousel></Carousel>      
+    //  <p></p>
     )
   }
 }
